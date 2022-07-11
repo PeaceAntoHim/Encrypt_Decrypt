d84aa83978b0a7e43ea06f71929eeceafb1847d3
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-07-12 00:21:30 +0700

    initial commit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+    val secretKey: String =
+        "662ede816988e58fb6d057d9d85605e0"
+
+    val encryptor = AESEncryptorGCM()
+
+    val encryptedValue: String? =encryptor.encrypt("Rohail", secretKey)
+    println(encryptedValue)
+
+    val decryptedValue: String? =encryptor.decryptWithAES(secretKey, encryptedValue)
+    println(decryptedValue)
+}
